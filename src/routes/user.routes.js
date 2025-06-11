fa1ddc2c234bd4f7acef6d127e967a96e2b3305f
Er-Kapil <157969010+Er-Kapil@users.noreply.github.com>
2025-06-11 23:55:41 +0530

    Fix token generation function and add refresh token endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,7 @@ import upload from '../middlewares/multer.middleware.js'
 import {loginUser} from '../controllers/user.controller.js'
 import {logoutUser} from '../controllers/user.controller.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
+import {refreshAccessToken} from '../controllers/user.controller.js'
 
 const router = Router()
 
@@ -26,5 +26,6 @@ router.route("/login").post(loginUser)
 
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
