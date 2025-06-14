29dc83d685aef45811d89e724f4d82f2c8dc0563
Er-Kapil <157969010+Er-Kapil@users.noreply.github.com>
2025-06-14 14:03:57 +0530

    Fix getUserChannelProfile to correctly call toLowerCase on username

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -320,7 +320,7 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
     const channel = await User.aggregate([
         {
             $match:{
-                username:username?.toLowerCase
+                username:username?.toLowerCase()
             }
         },
         {
